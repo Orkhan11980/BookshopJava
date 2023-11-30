b161eb7b7a6565b3c71b04a8d94add5bc8013dc2
Orkhan11980 <115529584+Orkhan11980@users.noreply.github.com>
2023-11-30 22:04:34 +0400

    java class to connect for db

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+
+public class Main {
+    public static void main(String[] args) {
+        Connection connection = Bookshop.connect();
+        if (connection != null) {
+            try {
+                connection.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
